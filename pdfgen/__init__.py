a7f13dad1afc90bcc92cfd40235e15910ccbaa84
Jan Fabry <jan.fabry@mobilevikings.com>
2012-06-28 10:00:48 +0200

    Use automatic Git-based versioning

--- pdfgen/__init__.py
+++ pdfgen/__init__.py
@@ -1,11 +1,23 @@
-VERSION = (1, 1, 0)
+import datetime
+import os
+import subprocess
 
+def get_git_version():
+    git_dir = os.path.abspath(
+        os.path.join(
+            os.path.abspath(os.path.dirname(__file__)),
+            '..',
+            '.git'
+        )
+    )
+    try:
+        # Python 2.7 has subprocess.check_output
+        # 2.6 needs this longer version
+        git_info = subprocess.Popen(['git', '--git-dir=%s' % git_dir, 'log', '--pretty=%ct %h', '-1'], stdout=subprocess.PIPE).communicate()[0].split()
+        git_time = datetime.datetime.fromtimestamp(float(git_info[0]))
+    except Exception:
+        git_time = datetime.datetime.now()
+        git_info = ('', '0000000')
+    return git_time.strftime('%Y.%m.%d') + '.' + git_info[1]
 
-def get_version():
-    if len(VERSION) > 3 and VERSION[3] != 'final':
-        return '%s.%s.%s %s' % (VERSION[0], VERSION[1], VERSION[2], VERSION[3])
-    else:
-        return '%s.%s.%s' % (VERSION[0], VERSION[1], VERSION[2])
-
-
-__version__ = get_version()
+__version__ = get_git_version()
